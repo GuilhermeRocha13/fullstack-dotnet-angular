0580e1b8165c85ebbd78f64fbbaf39f1563a3095
Guilherme Henrique <guilherme_h2006@hotmail.com>
2024-04-26 08:16:47 -0300

    models

---
+++ Back/src/ProEvent.API/Controllers/EventoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEvent.API.Models;
+
+namespace ProEvent.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public Evento Get()
+        { 
+            return new Evento(){ //as propriedades do novo objeto é definido entre chaves
+                EventoID = 1
+            };
+        }
+
+
+        [HttpPost]
+
+        public string Post(){
+            return "post";
+        }
+        
+        [HttpPut("{id}")]
+
+        public string Post(int id){
+            return "post" + id;
+        }
+    }
+}
---
+++ Back/src/ProEvent.API/Models/Evento.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEvent.API.Models
+{
+    public class Evento
+    {
+        public int EventoID { get; set; }
+
+        public string local { get; set; }
+    }
+}
