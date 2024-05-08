bf20f3ecb0769776fa462bc2d3055ba6a7a7a98c
Guilherme Henrique <guilherme_h2006@hotmail.com>
2024-05-08 13:58:11 -0300

    import http

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { RouterOutlet } from '@angular/router';
 import { EventosComponent } from './eventos/eventos.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PalestrantesComponent, EventosComponent],
+  imports: [RouterOutlet, PalestrantesComponent, EventosComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
